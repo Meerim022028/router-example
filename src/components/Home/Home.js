46f75be2fa615a6f1d57a3acd03ec8da7ed46125
Meerim Toktorova <meritoktorova@mail.com>
2021-04-14 16:40:02 +0600

    added h1 and p

---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import classes from "./Home.module.css";
+
+
+const Home = ({  }) => {
+  
+  return (
+    <div className={classes.Home}>
+     <h1>Home</h1> 
+     <p>Hello world</p>
+     
+    </div>
+  );
+}
+
+export default Home;
