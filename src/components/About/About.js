914795cafe8f814c944b93034da43b5e7ca40d28
Meerim Toktorova <meritoktorova@mail.com>
2021-04-14 16:39:25 +0600

    added h1 and p

---
+++ src/components/About/About.js
@@ -0,0 +1,15 @@
+import classes from "./About.module.css";
+
+
+const About = ({  }) => {
+  
+  return (
+    <div className={classes.About}>
+     <h1>About</h1> 
+     <p>lorem</p>
+     
+    </div>
+  );
+}
+
+export default About;
