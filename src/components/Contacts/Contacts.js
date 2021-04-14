b8479b946852318bf2337c6cc01c6f7aef6c0aed
Meerim Toktorova <meritoktorova@mail.com>
2021-04-14 16:39:56 +0600

    added h1 and p

---
+++ src/components/Contacts/Contacts.js
@@ -0,0 +1,15 @@
+import classes from "./Contacts.module.css";
+
+
+const Contacts = ({  }) => {
+  
+  return (
+    <div className={classes.Contacts}>
+     <h1>Contacts</h1> 
+     <p>asdfghj</p>
+     
+    </div>
+  );
+}
+
+export default Contacts;
