c79dc1a550e2f4b768964dcdf65fa2712b1ea54c
Meerim Toktorova <meritoktorova@mail.com>
2021-04-14 16:39:03 +0600

    Changes App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import Home from "./components/Home";
-import About from "./components/About";
-import Contacts from "./components/Contacts";
 import './App.css';
+import About from "./components/About/About";
+import Contacts from "./components/Contacts/Contacts";
+import Home from "./components/Home/Home";
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <a href="/" >home</a>
+      <a href="/bout" >About</a>
+      <a href="/contacts" >Contacts</a>
       <About />
       <Contacts />
+      <Home />
     </div>
   );
 }
