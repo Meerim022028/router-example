39e62a28eae4a2ea2156599ef8a8d6f2c3c52118
Meerim Toktorova <meritoktorova@mail.com>
2021-04-15 14:48:56 +0600

    added new components "By"

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { Route } from 'react-router';
+import { Route, Switch } from 'react-router';
 import './App.css';
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Contacts from "./components/Contacts/Contacts";
-import { Link } from 'react-router-dom';
+import By from "./components/By/By";
+import { NavLink } from 'react-router-dom';
 
 
 
@@ -11,14 +12,29 @@ import { Link } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <Link to="/" >Home</Link>
-      <Link to="/about" >About</Link>   
-      <Link to="/contacts" >Contacts</Link>
-    
+      <ul>
+        <li>
+          <NavLink activeClassName="acive" to="/" exact>Home</NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName="acive" to="/about" >About</NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName="acive" to="/contacts" >Contacts</NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName="acive" to="/by" >By</NavLink>
+        </li>
+      </ul>
+
+    <Switch>
       <Route path="/" component={Home} exact/>
-      <Route path="/about" component={About} />
-      <Route path="/contacts" component={Contacts} />
-      
+      <Route path="/about" component={About}  /> 
+      <Route path="/contacts" component={Contacts}  />
+      <Route path="/by" component={By} />
+      <Route path="/" render={() => <h1>Page not Found</h1>} />
+       {/* <Redirect to="/" /> returns to the first page*/}
+    </Switch>
     </div>
   );
 }
