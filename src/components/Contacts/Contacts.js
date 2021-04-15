d092b4a7b4f22c9459f9035159ae23937191cd98
Meerim Toktorova <meritoktorova@mail.com>
2021-04-15 16:02:14 +0600

    change contacts.js

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,13 +1,20 @@
 import classes from "./Contacts.module.css";
+import {Link, Route, Switch} from "react-router-dom";
+
 
 
 const Contacts = ({  }) => {
   
   return (
     <div className={classes.Contacts}>
-     <h1>Contacts</h1> 
-     <p>asdfghj</p>
+     <Link to="/contacts/personal">Personal contacts</Link>
+     <Link to="/contacts/business">Personal contacts</Link>
      
+
+     <Switch>
+    <Route path="/contacts/personal" render={() => <h1>Personal Contacts</h1>}  />
+    <Route path="/contacts" render={() => <h1>Business Contacts</h1>}  />
+    </Switch>
     </div>
   );
 }
