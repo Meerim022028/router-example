7bba863a27d0f231f58807b95590b2747c901652
Meerim Toktorova <meritoktorova@mail.com>
2021-04-15 16:53:00 +0600

    xcv

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -7,10 +7,12 @@ const Contacts = ({  }) => {
   
   return (
     <div className={classes.Contacts}>
+
+
+
      <Link to="/contacts/personal">Personal contacts</Link>
      <Link to="/contacts/business">Personal contacts</Link>
      
-
      <Switch>
     <Route path="/contacts/personal" render={() => <h1>Personal Contacts</h1>}  />
     <Route path="/contacts" render={() => <h1>Business Contacts</h1>}  />
