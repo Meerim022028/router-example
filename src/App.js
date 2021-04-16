7595f0d61c9f6f4afc2ffbd226f4be5d01ea0e04
Meerim Toktorova <meritoktorova@mail.com>
2021-04-16 10:42:06 +0600

    chnges App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Contacts from "./components/Contacts/Contacts";
 import Posts from "./components/Posts/Posts";
+import By from "./components/By/By"
+import Author from "./components/Author/Author"
+import Page from "./components/Page/Page"
+import Linc from "./components/Linc/Linc";
+import Search from "./components/Search/Search";
+import Main from "./components/Main/Main";
+
+
+
+
 import { NavLink } from 'react-router-dom';
 
 
@@ -25,7 +35,24 @@ function App() {
         <li>
           <NavLink activeClassName="acive" to="/posts" >Posts</NavLink>
         </li>
-        
+        <li>
+          <NavLink activeClassName="acive" to="/by" >By</NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName="acive" to="/author" >Author</NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName="acive" to="/page" >Page</NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName="acive" to="/linc" >linc</NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName="acive" to="/search" >Search</NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName="acive" to="/main" >Main</NavLink>
+        </li>
       </ul>
 
     <Switch>
@@ -33,6 +60,17 @@ function App() {
       <Route path="/about" component={About}  /> 
       <Route path="/contacts" component={Contacts}  />
       <Route path="/posts" component={Posts} />
+      <Route path="/by" component={By} />
+      <Route path="/author" component={Author} />
+      <Route path="/page" component={Page} />
+      <Route path="/linc" component={Linc} />
+      <Route path="/search" component={Search} />
+      <Route path="/main" component={Main} />
+
+
+
+
+
       <Route path="/" render={() => <h1>Page not Found</h1>} />
        {/* <Redirect to="/" /> returns to the first page*/}
     </Switch>
