4412d520c1117d6aae07409030117c6b0f9c046d
Meerim Toktorova <meritoktorova@mail.com>
2021-04-16 10:44:31 +0600

    changes

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -11,11 +11,25 @@ const Contacts = ({  }) => {
 
 
      <Link to="/contacts/personal">Personal contacts</Link>
-     <Link to="/contacts/business">Personal contacts</Link>
+     <Link to="/contacts/business">Business contacts</Link>
      
      <Switch>
-    <Route path="/contacts/personal" render={() => <h1>Personal Contacts</h1>}  />
-    <Route path="/contacts" render={() => <h1>Business Contacts</h1>}  />
+    <Route path="/contacts/personal" render={() => <h1>Personal Contacts
+     
+
+      <div className={classes.div1}>
+<aside>Jon Bess</aside>
+<article>p:-098-567-876-08</article>
+<article>str:132-Sietl</article>
+</div> </h1>}  />
+    <Route path="/contacts" render={() => <h1>Business Contacts
+      
+<div className={classes.div2}>
+<aside>Timi Rower</aside>
+<article>p:-567-987-98-08</article>
+<article>str:789-Sietl</article>
+</div>
+    </h1>}  />
     </Switch>
     </div>
   );
